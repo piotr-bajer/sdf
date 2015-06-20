7998189e95aec37154424002bb6b712ba35440b3
Steve Avery <schavery@gmail.com>
2015-06-19 18:07:46 -0700

    move messager into own file

---
+++ message.php
@@ -0,0 +1,62 @@
+<?php
+/**
+message handler
+*/
+
+require_once WP_PLUGIN_DIR . '/sdf/types.php';
+
+function sdf_message_handler($type, $message) {
+	// type = (ERROR | SUCCESS | LOG)
+	// all messages are written to sdf.log
+	// rotated every six months
+	// success and error messages are passed as an object to the waiting js
+	// the data structure is json, and should be very simple.
+	// data.type = error | success
+	// data.message = message
+
+	switch($type) {
+		case MessageTypes::ERROR: $type = 'error'; break;
+		case MessageTypes::SUCCESS: $type = 'success'; break;
+		case MessageTypes::LOG: $type = 'log'; break;
+	}
+
+	$logmessage = time() . ' - ' . $type . ' - ' . $message . "\n";
+	file_put_contents(WP_PLUGIN_DIR . '/sdf/sdf.log', $logmessage, FILE_APPEND);
+
+	// send data to the requestor
+	if($type != 'log') {
+		ob_clean();
+		$data = array(
+			'type' => $type,
+			'message' => $message
+		);
+		echo json_encode($data);
+		ob_flush();
+		die();
+	}
+}
+
+function sdf_clean_log() {
+	$file = WP_PLUGIN_DIR . '/sdf/sdf.log';
+	$handle = fopen($file, 'r+');
+	$linecount = 0;
+	while(!feof($handle)) {
+		$line = fgets($handle);
+		$linecount++;
+	}
+	ftruncate($handle, 0);
+	rewind($handle);
+	fwrite($handle, time() . ' - Cron run. ' . $linecount . ' lines cleared.' . "\n");
+	fclose($handle);
+}
+
+function sdf_add_biannual($schedules) {
+	$schedules['biannual'] = array(
+		'interval' => 10000000,
+		'display' => __('Every Six Months')
+	);
+	return $schedules;
+}
+
+add_filter('cron_schedules', 'sdf_add_biannual');
+add_action('sdf_biannual_hook', 'sdf_clean_log'); ?>
