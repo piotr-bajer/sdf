9b5ff6866e25beaa2eef3a46ba8e10d36599e577
Steve Avery <schavery@gmail.com>
2015-10-14 22:01:40 -0700

    Add test cases description

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Test cases to cover:
+Annual
+Monthly
+One-time
+custom
+new customer
+new company
+referrer
+
+in honor of on first donation
+in honor of on recurring donation
+
+multiple subscriptions
+	different in honor ofs
+
+*/
